644257a6330b3a80a7ce80149f9b40fdbe8b7faf
Sergio Castillo Gutierrez <daw04.2022@gmail.com>
2024-01-29 09:58:40 +0100

    added models and more business logic

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Recipe} from "../../../model/Recipe";
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.scss']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('arepas', 'son de vnzla', 'https://cdn.recetasderechupete.com/wp-content/uploads/2019/11/Tipos-de-arepa-2.jpg')
+  ];
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.getRecipes();
+  }
+
+  public getRecipes() {
+    const recipes = this.recipes;
+    console.log(recipes);
+    return recipes;
+  }
+
 
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Ingredient} from "../../model/Ingredient";
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.scss']
 })
-export class ShoppingListComponent {
+export class ShoppingListComponent implements OnInit {
+  ingridients: Ingredient[] = [
+    new Ingredient('ingredient 1', 12),
+    new Ingredient('ingredient 2', 2)
+  ];
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.getIngredientes();
+  }
+
+  public getIngredientes() {
+    const ingredientes = this.ingridients;
+    console.log(ingredientes);
+    return ingredientes;
+  }
+
 
 }
